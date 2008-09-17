0dac64c27f83caf8aa2d6d68c08ac04094819b15
dragotin <freitag@suse.de>
2008-09-17 10:00:03 +0000

    ability to work with multiple track segments
    git-svn-id: svn+ssh://svn.kde.org/home/kde/branches/work/~tpatzig/activitydiary@861847 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/gpxparser.cpp
+++ src/gpxparser.cpp
@@ -8,12 +8,12 @@ GPXParser::GPXParser(QString filename) {
 	else
 		qDebug() <<"Can't open " << filename << " for reading.";
 	wpt = 0;
-	
-	
+
+
 }
 
 GPXParser::~GPXParser() {
-	if (wpt) 
+	if (wpt)
 		delete wpt;
 
   qDeleteAll(waypoint_list.begin(),waypoint_list.end());
@@ -31,55 +31,58 @@ void GPXParser::parse_file(QFile &file) {
 
   minLat = 0;
   minLon = 0;
-  
 
-  QDomElement trkpt = mainElement.firstChildElement("trk").firstChildElement("trkseg").firstChildElement("trkpt");
-     for (; !trkpt.isNull(); trkpt = trkpt.nextSiblingElement("trkpt")) {
-            
-            double lat = trkpt.attributeNode("lat").value().toDouble();
-            double lon = trkpt.attributeNode("lon").value().toDouble();
-            float alt = trkpt.firstChildElement("ele").text().toFloat();
 
-            // Search the most east, west, north and south point //
-            if (lat > maxLat) {
-              maxLat = lat;
-              maxNorth.setY(lat);
-              maxNorth.setX(lon);
-            }  
+  QDomElement trkseg = mainElement.firstChildElement("trk").firstChildElement("trkseg");
+  for ( ; !trkseg.isNull(); trkseg = trkseg.nextSiblingElement( "trkseg" ) ) {
+        QDomElement trkpt = trkseg.firstChildElement("trkpt");
+        for (; !trkpt.isNull(); trkpt = trkpt.nextSiblingElement("trkpt")) {
 
-            if (lon > maxLon) {
-              maxLon = lon;
-              maxEast.setY(lat);
-              maxEast.setX(lon);
-            }
-            
-            if (minLat == 0 || lat < minLat) {
-              minLat = lat;
-              maxSouth.setY(lat);
-              maxSouth.setX(lon);
-            }
+                double lat = trkpt.attributeNode("lat").value().toDouble();
+                double lon = trkpt.attributeNode("lon").value().toDouble();
+                float alt = trkpt.firstChildElement("ele").text().toFloat();
 
-            if (minLon == 0 || lon < minLon) {
-              minLon = lon;
-              maxWest.setY(lat);
-              maxWest.setX(lon);
-            }
+                // Search the most east, west, north and south point //
+                if (lat > maxLat) {
+                  maxLat = lat;
+                  maxNorth.setY(lat);
+                  maxNorth.setX(lon);
+                }
 
-            QString date_time_string =  trkpt.firstChildElement("time").text();
-            QStringList date_list = date_time_string.split('T')[0].split('-');
-            QStringList time_list = date_time_string.split('T')[1].split('Z')[0].split(':');
+                if (lon > maxLon) {
+                  maxLon = lon;
+                  maxEast.setY(lat);
+                  maxEast.setX(lon);
+                }
 
-            QDate date(date_list[0].toInt(),date_list[1].toInt(),date_list[2].toInt());
-            QTime time(time_list[0].toInt(),time_list[1].toInt(),time_list[2].toInt());
-            QDateTime date_time(date,time);
+                if (minLat == 0 || lat < minLat) {
+                  minLat = lat;
+                  maxSouth.setY(lat);
+                  maxSouth.setX(lon);
+                }
 
-            int sat = trkpt.firstChildElement("sat").text().toInt();
-            float speed =  trkpt.firstChildElement("extensions").firstChildElement("rmc:speed").text().toFloat();
-            float course = trkpt.firstChildElement("extensions").firstChildElement("rmc:course").text().toFloat();
-      
-            wpt = new Waypoint( lat,lon,sat,alt,speed,course,date_time );
-            add_waypoint(wpt);
-      }
+                if (minLon == 0 || lon < minLon) {
+                  minLon = lon;
+                  maxWest.setY(lat);
+                  maxWest.setX(lon);
+                }
+
+                QString date_time_string =  trkpt.firstChildElement("time").text();
+                QStringList date_list = date_time_string.split('T')[0].split('-');
+                QStringList time_list = date_time_string.split('T')[1].split('Z')[0].split(':');
+
+                QDate date(date_list[0].toInt(),date_list[1].toInt(),date_list[2].toInt());
+                QTime time(time_list[0].toInt(),time_list[1].toInt(),time_list[2].toInt());
+                QDateTime date_time(date,time);
+
+                int sat = trkpt.firstChildElement("sat").text().toInt();
+                float speed =  trkpt.firstChildElement("extensions").firstChildElement("rmc:speed").text().toFloat();
+                float course = trkpt.firstChildElement("extensions").firstChildElement("rmc:course").text().toFloat();
+
+                wpt = new Waypoint( lat,lon,sat,alt,speed,course,date_time );
+                add_waypoint(wpt);
+       }
+  }
 }
 
 bool GPXParser::open_file(QFile &file) {
@@ -95,9 +98,9 @@ int GPXParser::count_waypoints() {
 void GPXParser::add_waypoint(Waypoint* wpt) {
   waypoint_list.append(wpt);
 }
-    
+
 QPointF GPXParser::max_east() {
-    return maxEast;     
+    return maxEast;
 }
 
 QPointF GPXParser::max_west() {
